f15b03d162533fcb6c4dfe5862cbce028a3aa8f0
geekrun <11956884+geekrun@users.noreply.github.com>
2025-06-16 22:24:53 +0800

    Refactor WebSocket handling to use MessageContext and improve error handling
    Replaced direct `ChannelHandlerContext` references with `MessageContext` for better session management, making the system more flexible. Improved error handling for invalid or malformed messages and added detailed logging for debugging. Updated `MessageQueue` to remove unused `poll()` method and streamlined session management in `WebSocketSessionManager`.

--- mass_server/src/main/java/com/xa/mass/server/MassServerApplication.java
+++ mass_server/src/main/java/com/xa/mass/server/MassServerApplication.java
@@ -14,20 +14,6 @@ public class MassServerApplication {
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(MassServerApplication.class, args);
 
-        // 检查 WebSocketMessageProcessor 是否已加载并初始化
-        try {
-            WebSocketMessageProcessor processor = context.getBean(WebSocketMessageProcessor.class);
-            if (processor != null) {
-                logger.info("✅ WebSocketMessageProcessor bean is successfully loaded and available in the application context.");
-            } else {
-                // 这段逻辑实际上不会执行，因为如果bean不存在，getBean会抛异常
-                logger.info("WebSocketMessageProcessor bean is not loaded");
-            }
-        } catch (Exception e) {
-            logger.error("❌ Failed to retrieve WebSocketMessageProcessor bean from the application context.", e);
-        }
-
-
         logger.info("MassServerApplication started");
     }
 }
