56e5e9d1f29ef6a84d1bd539e888d01ddb0e80a7
quiaingmuarua <2424619166@qq.com>
2026-05-27 18:15:50 +0800

    Keep candidate diagnostics off runtime contract

--- xa-mass-engine/src/main/java/com/xa/mass/engine/worker/WorkerCandidateRuntime.java
+++ xa-mass-engine/src/main/java/com/xa/mass/engine/worker/WorkerCandidateRuntime.java
@@ -4,16 +4,10 @@ import com.xa.mass.base.model.Worker;
 import com.xa.mass.runtime.worker.WorkerCandidateBatch;
 import com.xa.mass.runtime.worker.WorkerTaskSelector;
 
-import java.util.List;
-
 /**
  * Candidate-source surface for worker matching.
  */
 public interface WorkerCandidateRuntime {
-
-    List<Worker> findWorkerCandidates(WorkerTaskSelector selector);
-
-    List<Worker> findWorkerCandidates(WorkerTaskSelector selector, int maxCandidateCount);
 
     WorkerCandidateBatch<Worker> findWorkerCandidateBatch(WorkerTaskSelector selector, int maxCandidateCount);
 
